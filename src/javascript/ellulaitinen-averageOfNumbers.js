7e9053e30459219f2f9f45c1d91df88caf3f88bb
ElluLaitinen <ellu.laitinen85@gmail.com>
2020-10-11 13:24:53 +0300

    add averageOfNumbers in JavaScript

---
+++ src/javascript/ellulaitinen-averageOfNumbers.js
@@ -0,0 +1,15 @@
+// This function calculates the average of numbers
+
+const numbers = [5, 8, -7, 3, -2, 15, 8, 6];
+
+const avgOfNumbers = () => {
+    const sum = numbers.reduce((a, b) => {
+        return a + b;
+    }, 0);
+
+    const average = sum / numbers.length;
+
+    return average;
+}
+
+avgOfNumbers()

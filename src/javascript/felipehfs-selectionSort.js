6eb8212d6cac2893037125868e2847a3a88d32df
Felipe Henrique <felipehfsouza@gmail.com>
2020-10-13 20:11:07 -0300

    Add selectionSort in javascript

---
+++ src/javascript/felipehfs-selectionSort.js
@@ -0,0 +1,19 @@
+
+function selectionSort(array) {
+    let newArray = [...array];
+    
+    let min = 0;
+    for (let i = 0; i < newArray.length - 1; i++) {
+        for(let j = i + 1; j < newArray.length; j++) {
+            if (newArray[min] > newArray[j]) {
+                min = j;
+            }
+        }
+
+        const temp = newArray[i];
+        newArray[i] = newArray[min];
+        newArray[min] = temp;
+    }
+
+    return newArray;
+}
